7bfeef3bef980b0f9bfcca30f7f0dd7b35724df8
Pbernal30980 <pbernal309@gmail.com>
2025-06-20 16:48:54 -0500

    Initial commit

---
+++ src/main/java/com/example/gtu_monitoring_log_service/GtuMonitoringLogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gtu_monitoring_log_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtuMonitoringLogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtuMonitoringLogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gtu_monitoring_log_service/GtuMonitoringLogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtu_monitoring_log_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtuMonitoringLogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

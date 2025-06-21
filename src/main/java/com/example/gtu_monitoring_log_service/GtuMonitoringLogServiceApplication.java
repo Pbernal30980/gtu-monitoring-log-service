63897daefaf1ec4031702b32785a3575f1dfd0ca
SantiReRo <santiago.reyes.rodriguez@correounivalle.edu.co>
2025-06-20 22:08:39 -0500

    Implement mongodb

--- src/main/java/com/example/gtu_monitoring_log_service/GtuMonitoringLogServiceApplication.java
+++ src/main/java/com/example/gtu_monitoring_log_service/GtuMonitoringLogServiceApplication.java
@@ -1,9 +1,11 @@
 package com.example.gtu_monitoring_log_service;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class GtuMonitoringLogServiceApplication {
 
 	public static void main(String[] args) {
